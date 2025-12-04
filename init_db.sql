0cff03921f40d64cf61865d1e6f9e6820b28cb86
Gridness <61116326+Gridness@users.noreply.github.com>
2025-12-04 10:47:42 +0300

    KAN-2 Update dependencies and remove aiosqlite from rbac and reporting services
    - Removed "aiosqlite" dependency from pyproject.toml and uv.lock in both rbac_service and reporting_service.
    - Added "asyncpg" dependency to pyproject.toml and uv.lock in both services.
    - Updated the version of "shared-messages" in reporting_service.
    - Created init_db.sql to set up initial databases for the application.

---
+++ init_db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE phi_appointments;
+CREATE DATABASE phi_audit;
+CREATE DATABASE phi_ehr;
+CREATE DATABASE phi_billing;
+CREATE DATABASE phi_notification;
+CREATE DATABASE phi_patient;
+CREATE DATABASE phi_rbac;
+CREATE DATABASE phi_reporting;
